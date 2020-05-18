e9e1cd8dcd2ba73b376c89f21e94579a89a9a85d
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2020-05-18 09:29:32 +0600

    add files
    some basic files which shows the arithmetic operation

---
+++ basic-arith.py
@@ -0,0 +1,14 @@
+a = input("Enter a value:")
+b = input("Enter another value:")
+
+add = float(a) + float(b)
+print("Addition of two value is:",add)
+
+sub = float(a) - float(b)
+print("Substraction of two value is:",sub)
+
+mul = float(a) * float(b)
+print("Multiplicaton of two value is:",mul)
+
+div = float(a) / float(b)
+print("Divition of two value is:",div)
---
+++ basic-mul.py
@@ -0,0 +1,8 @@
+hrs = input("Enter Hours:")
+rate = input("Enter Rates:")
+
+hours = int(hrs)
+rates = float(rate)
+
+pay = (hours*rates)
+print("Pay:",pay)
