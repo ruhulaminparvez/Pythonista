a7fd7511cc68a51ad540fd4ad24f6cf4ee20b986
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2020-05-20 22:51:50 +0600

    Add files to Basic

--- Basic/basic-arith.py
+++ Basic/basic-arith.py
@@ -2,13 +2,22 @@ a = input("Enter a value:")
 b = input("Enter another value:")
 
 add = float(a) + float(b)
-print("Addition of two value is:",add)
+print("Addition of two values are:",add)
 
 sub = float(a) - float(b)
-print("Substraction of two value is:",sub)
+print("Substraction of two values are:",sub)
 
 mul = float(a) * float(b)
-print("Multiplicaton of two value is:",mul)
+print("Multiplicaton of two values are:",mul)
 
 div = float(a) / float(b)
-print("Divition of two value is:",div)
+print("Divition of two values are:",div)
+
+modu = float(a) % float(b)
+print("Modulus of two values are:",modu)
+
+fdiv = float(a) // float(b)
+print("Floor Division of two values are:",fdiv)
+
+expo = float(a) ** float(b)
+print("Exponent of two values are:",expo)
---
+++ Basic/basic-bitwise.py
@@ -0,0 +1,7 @@
+x = input("enter 1st value:")
+y = input("enter 2nd value:")
+
+x = float(x)
+y = float(y)
+
+print("Bitwise AND :",x&y)
