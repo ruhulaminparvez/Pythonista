a83a6f46ac459cbe4490aa32db05332b7f0e190c
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2020-05-28 15:50:13 +0600

    Swap & Add String :snake:

---
+++ String/At-the-end-add-string.py
@@ -0,0 +1,18 @@
+''' Write a Python program to add 'ing'
+at the end of a given string (length should be at least 3).
+If the given string already ends with 'ing' then add 'ly' instead.
+If the string length of the given string is less than 3, leave it unchanged.'''
+
+s = (str(input("Enter a string: ")))
+
+def add_string(str1):
+  length = len(str1)
+
+  if length > 2:
+    if str1[-3:] == 'ing':
+      str1 += 'ly'
+    else:
+      str1 += 'ing'
+
+  return str1
+print("Output of string: ",add_string(s))
