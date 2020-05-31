f24074e4bae5ce27db3953155fbe543e19caa919
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2020-05-31 20:09:39 +0600

    Ceasar Code | Float :snake:

---
+++ String/float.py
@@ -0,0 +1,6 @@
+"""Write a Python program to print the following floating numbers upto 2 decimal places."""
+
+f1 = (float(input("enter a float value: ")))
+
+print("float value upto 2 decimal place is: %.2f"%(f1))
+
