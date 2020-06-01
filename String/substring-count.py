2d6e1735c299fc88b869289f15e8f43beb0bcfb0
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2020-06-01 14:57:35 +0600

    reverse string count 🐸

---
+++ String/substring-count.py
@@ -0,0 +1,9 @@
+"""Write a Python program to count occurrences of a substring in a string"""
+
+str1 = input("enter a string: ")
+
+print("written string line is: ",str1)
+print(str1.count(input("substring for count: ")))
+
+
+
