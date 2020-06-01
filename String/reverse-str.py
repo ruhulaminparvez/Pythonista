22b69de3da971a452681251e3328156729568dea
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2020-06-01 14:58:22 +0600

    reverse string 🐸

--- String/reverse-str.py
+++ String/reverse-str.py
@@ -1,10 +1,10 @@
-#Write a Python function to reverses a string if it's length is a multiple of 4.
+"""Write a Python program to reverse a string"""
 
-s = (input("enter a string: "))
+s = input("enter a string: ")
 
 def reverse_string(str1):
-    if len(str1) % 4 == 0:
-       return ''.join(reversed(str1))
-    return "no change!!",str1
+    return ''.join(reversed(str1))
 
 print("reverse string is: ",reverse_string(s))
+
+
