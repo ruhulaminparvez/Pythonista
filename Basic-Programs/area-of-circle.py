bf107519c256a49ac499970d7fa844614d95d807
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2020-06-02 19:05:43 +0600

    area of circle 🤞🐍

---
+++ Basic-Programs/area-of-circle.py
@@ -0,0 +1,11 @@
+"""Python Program for Program to find area of a circle"""
+
+r = (float(input("enter radius value: ")))
+
+pi = 3.1416 
+
+area = pi * (r*r)
+
+print("area of a circle is: ",area)
+
+
