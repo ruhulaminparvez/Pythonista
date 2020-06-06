ec0482894c0434dc91841ad01c42d3c51a3dd581
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2020-06-06 22:35:21 +0600

    reverse name 🐍

---
+++ Basic_Part-1/reverse-name.py
@@ -0,0 +1,7 @@
+"""Write a Python program which accepts the user's first and
+last name and print them in reverse order with a space between them."""
+
+fname = input("enter first name of your's: ")
+lname = input("enter last name of your's: ")
+
+print("reverse name is: ",lname,fname)
