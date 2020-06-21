9ba2341afc31e896b906394aa0d84ad0e7ae2144
ruhul amin parvez <ruhulaminparvez007@gmail.com>
2020-06-22 03:38:54 +0600

    Command Line Arguments :honeybee:

---
+++ learn-python/command-line-arguments/command-line.py
@@ -0,0 +1,10 @@
+"""Display the command line argument, argument length, Script/Program Name"""
+
+import sys
+
+lst = sys.argv
+for i in lst: print(lst)
+
+print(len(lst))
+
+print(lst[0])
---
+++ learn-python/command-line-arguments/product.py
@@ -0,0 +1,6 @@
+# Product Two Numbers
+
+import sys
+
+lst = sys.argv
+print("Product is:",int(lst[1])*int(lst[2]))
