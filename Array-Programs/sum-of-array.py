a123d1c7c67df9dad77e5c797d58842189fdc5b9
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2021-01-25 20:03:48 +0600

    Sum Of Array ✅

---
+++ Array-Programs/sum-of-array.py
@@ -0,0 +1,13 @@
+# Python 3 code to find sum of elements in given array
+arr = []
+
+# input values to list
+arr = [12, 3, 4, 15, 6]
+
+# sum() is an inbuilt function in python that adds
+# all the elements in list,set and tuples and returns
+# the value
+ans = sum(arr)
+
+# display sum
+print ('Sum of the array is ',ans)
