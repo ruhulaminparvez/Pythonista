12e8207cc42a28d2b05f305d293f170d7cc35cbb
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2021-03-05 13:55:52 +0600

    Nested List 🗽

---
+++ List/nested-list.py
@@ -0,0 +1,18 @@
+record = []
+second_lowest_names = []
+scores = set()
+
+for _ in range(int(input())):
+    name = input()
+    score = float(input())
+    record.append([name, score])
+    scores.add(score)
+        
+second_lowest = sorted(scores)[1]
+
+for name, score in record:
+    if score == second_lowest:
+        second_lowest_names.append(name)
+
+for name in sorted(second_lowest_names):
+    print(name, end='\n')
